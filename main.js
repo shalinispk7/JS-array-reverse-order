5994b9aeb9a70fb3030b703d3a78152036b1120d
shalinispk7 <shalinispk7@gmail.com>
2023-05-31 00:05:08 +0530

    array reverse order

---
+++ main.js
@@ -0,0 +1,35 @@
+
+let arr=[4,6,8];
+document.write("The value store in array are:");
+document.write("<br>");
+for (i=0;i<arr.length;i++){
+	document.write(arr[i]+" ");
+}
+document.write("<br>");
+document.write("<br>");
+
+// array reverse order
+let array=[2,3,5,8,21,44];
+document.write("The value store in array are:");
+document.write("<br>");
+for (i=0;i<array.length;i++){
+	document.write(array[i]+" ");
+}
+document.write("<br>");
+document.write("The value store in array are in reverse order: ");
+for (i=(array.length-1);i>=0;i--){
+	document.write(array[i]+" ");
+}
+		
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
